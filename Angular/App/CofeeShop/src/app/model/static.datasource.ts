f34cbeb8f0fdeea0f23a0ec86adf9ad0ae54353f
ash ketchum <ash@localhost.localdomain>
2021-08-26 09:41:28 +0200

    Adjusted data.json to data.js for module import

--- Angular/App/CofeeShop/src/app/model/static.datasource.ts
+++ Angular/App/CofeeShop/src/app/model/static.datasource.ts
@@ -2,12 +2,13 @@ import { Injectable } from "@angular/core";
 import { Product } from "./product.model";
 import { Category } from "./category.model";
 import { Observable, from } from "rxjs";
+import { default as data} from "../../../data.js";
+
 
 @Injectable()
 export class StaticDataSource {
-    private products: Product[] = [];
-
-    private categories: Category[] = [];
+    private products: Product[] = data.products;
+    private categories: Category[] = data.categories;
 
     getProducts(): Observable<Product[]> {
         return from([this.products]);
