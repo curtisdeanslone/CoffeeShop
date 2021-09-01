13d1ad4f01e2bdc7c67ddcd981af922363fb1c05
ash ketchum <ash@localhost.localdomain>
2021-09-01 11:16:39 +0200

    re-movement of files TODO: Fix admin and models to reflect categories and users

--- Angular/App/CofeeShop/src/app/admin/auth.guard.ts
+++ Angular/App/CofeeShop/src/app/admin/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
-import { AuthService } from "./auth.service";
+import { AuthService } from "../model/auth.service";
 
 @Injectable()
 export class AuthGuard {
