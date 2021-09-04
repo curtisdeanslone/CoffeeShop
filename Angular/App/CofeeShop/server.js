00b15d583c2935e5b23d2f779ff35d6080ce4d91
ash ketchum <ash@localhost.localdomain>
2021-09-04 21:10:30 +0200

    Final Push

--- Angular/App/CofeeShop/server.js
+++ Angular/App/CofeeShop/server.js
@@ -1,30 +1,35 @@
 const express = require("express");
-const http = require("https");
+const https = require("https");
 const fs = require("fs");
 const history = require("connect-history-api-fallback");
 const jsonServer = require("json-server");
 const bodyParser = require('body-parser');
 const auth = require("./authMiddleware");
-const router = jsonServer.router("server-data.json");
+const router = jsonServer.router("serverdata.json");
+
 const enableHttps = false;
-const ssloptions = {};
+
+const ssloptions = {}
 
 if (enableHttps) {
-    ssloptions.cert = fs.readFileSync("./ssl/coffeeShop.crt");
-    ssloptions.key = fs.readFileSync("./ssl/coffeeShop.pem");
+    ssloptions.cert =  fs.readFileSync("./ssl/sportsstore.crt");
+    ssloptions.key = fs.readFileSync("./ssl/sportsstore.pem");
 }
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(auth);
-app.use("/api",router);
+app.use("/api", router);
 app.use(history());
-app.use("/",express.static("./dist/CoffeeShop"));
-app.listen(8080, () => console.log("HTTP Server running on port 80"));
-    if (enableHttps){
-        https.createServer(ssloptions, app).listen(443, () =>
-            console.log("HTTP Server running on port 443"));
-    } else {
-        console.log("HTTPS disabled");
-    }
+app.use("/", express.static("./dist/SportsStore"));
 
+app.listen(8080, 
+    () => console.log("HTTP Server running on port 80"));
+
+if (enableHttps) {
+    https.createServer(ssloptions, app).listen(443,
+        () => console.log("HTTPS Server running on port 443"));
+} else {
+    console.log("HTTPS disabled")
+}
--- Angular/App/CofeeShop/src/app/model/connection.service.ts
+++ Angular/App/CofeeShop/src/app/model/connection.service.ts
@@ -4,21 +4,24 @@ import { Observable, Subject } from "rxjs";
 @Injectable()
 export class ConnectionService {
     private connEvents: Subject<boolean>;
-    constructor(){
+        
+    constructor() {
         this.connEvents = new Subject<boolean>();
-        window.addEventListener("online", (e) => this.handleConnectionChange(e));
-        window.addEventListener("offline",(e) => this.handleConnectionChange(e));
-     }
+        window.addEventListener("online", 
+            (e) => this.handleConnectionChange(e));
+        window.addEventListener("offline", 
+            (e) => this.handleConnectionChange(e));
+    }
 
-     private handleConnectionChange(event) {
-         this.connEvents.next(this.connected);
-     }
+    private handleConnectionChange(event) {
+        this.connEvents.next(this.connected);
+    }
+ 
+    get connected() : boolean {
+        return window.navigator.onLine;
+    }
 
-     get connected(): boolean {
-         return window.navigator.onLine;
-     }
-
-     get Changes() {
-         return this.connEvents;
-     }
+    get Changes(): Observable<boolean> {
+        return this.connEvents;
+    }
 }
